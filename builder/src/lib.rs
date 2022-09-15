5d2953e994e51616c8963060e44dedbd9dd99f9c
fumisaitou <fsaito@cy2sec.comm.eng.osaka-u.ac.jp>
2022-09-15 15:49:45 +0900

    test2

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -6,26 +6,11 @@ use syn::{parse_macro_input, DeriveInput};
 pub fn derive(input: TokenStream) -> TokenStream {
     // Parse the input tokens into a syntax tree
     let input = parse_macro_input!(input as DeriveInput);
+    let indent = input.ident;
 
     // Build the output, possibly using quasi-quotation
     let expanded = quote!{
-        pub strust CommandBuilder {
-            executable: Option<String>,
-            args: Option<Vec<String>>,
-            env: Option<Vec<String>>,
-            current_dir: Option<String>,
-        }
-
-        impl Command {
-            pub fn builder() -> CommandBuilder {
-                CommandBuilder {
-                    executable: None,
-                    args: None,
-                    env: None,
-                    current_dir: None,
-                }
-            }
-        }
+        
     };
 
     // Hand the output tokens back to the compiler
